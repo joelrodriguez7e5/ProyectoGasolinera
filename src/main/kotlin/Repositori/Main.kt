98d0adcb5726d6fa3052036bf6a83668cb1695c1
Joel Rodriguez <joel.rodriguez.7e5@itb.cat>
2022-10-17 18:26:47 +0200

    ...

--- src/main/kotlin/Repositori/Main.kt
+++ src/main/kotlin/Repositori/Main.kt
@@ -1,11 +1,16 @@
 import io.ktor.client.*
+import io.ktor.client.call.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.request.*
 import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 
-fun main() {
-
+@Serializable
+data class Gasolineras()
+suspend fun main() {
+    val gasolineras: Gasolineras = client().get("https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/EstacionesTerrestres/").body()
 }
 fun client () = HttpClient(CIO){
     install(ContentNegotiation) {
