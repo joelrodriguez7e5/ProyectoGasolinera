2548d02753d2f1b1f82a13369bf6e95b7cb2b2ed
Joel Rodriguez <joel.rodriguez.7e5@itb.cat>
2022-10-17 18:33:14 +0200

    ...

--- src/main/kotlin/Repositori/Api.kt
+++ src/main/kotlin/Repositori/Api.kt
@@ -4,12 +4,14 @@ import io.ktor.client.engine.cio.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.request.*
 import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 import java.sql.Timestamp
 
 @Serializable
-data class Gasolineras(val fecha: Timestamp, val)
+data class Gasolineras(@SerialName("Fecha")val fecha: Timestamp, @SerialName("ListaEESSPrecio")val listaEESSPrecio: ListaEESSPrecio)
+data class ListaEESSPrecio()
 suspend fun main() {
     val gasolineras: Gasolineras = client().get("https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/EstacionesTerrestres/").body()
 }
