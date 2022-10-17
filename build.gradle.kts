6e5ba9b54e0a67e217befa7edea7cfc23d3c1c51
Joel Rodriguez <joel.rodriguez.7e5@itb.cat>
2022-10-17 18:23:31 +0200

    ...

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,10 +10,21 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven( "https://jitpack.io")
 }
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
+    implementation("io.ktor:ktor-client-core:2.1.0")
+    implementation("io.ktor:ktor-client-cio:2.1.0")
+    implementation("io.ktor:ktor-client-content-negotiation:2.1.0")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:2.1.0")
+    implementation("io.github.pdvrieze.xmlutil:core-jvm:0.84.2")
+    implementation("io.github.pdvrieze.xmlutil:serialization-jvm:0.84.2")
+    implementation("app.softwork:kotlinx-serialization-csv:0.0.6")
+    implementation("app.softwork:kotlinx-serialization-flf:0.0.6")
+    implementation ("io.github.kotlin-telegram-bot.kotlin-telegram-bot:telegram:6.0.7")
 }
 
 tasks.test {
--- src/main/kotlin/Repositori/Main.kt
+++ src/main/kotlin/Repositori/Main.kt
@@ -1,3 +1,16 @@
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.json.Json
+
 fun main() {
 
+}
+fun client () = HttpClient(CIO){
+    install(ContentNegotiation) {
+        json(Json {
+            ignoreUnknownKeys = true
+        })
+    }
 }
